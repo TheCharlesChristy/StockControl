f077661ab04ab6a66a376d5c51c39c2aa74aac4a
Charles Christy <charleschristy325@gmail.com>
2025-11-22 14:40:19 +0000

    Reset

--- src/frontend/components/Login/js/enter_username.js
+++
@@ -1,8 +0,0 @@
-function get_username() {
-  const username = document.getElementById("username").value;
-  if (username === "") {
-    showPageMessage("Error: Please enter your username.", "error");
-    return null;
-  }
-  return username;
-}
--- src/frontend/components/Login/js/goto_login_btn.js
+++
@@ -1,6 +0,0 @@
-function on_goto_login_btn_click() {
-  goto('/pages/Login/html/Login.html', {
-    showLoader: true,
-    smooth: true
-  });
-}
